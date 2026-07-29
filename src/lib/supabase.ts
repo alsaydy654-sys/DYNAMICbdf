b9658fb3309e6abcc2fee746ae7b2492ad7d5d7b
مكي <alsaydy654@gmail.com>
2026-07-29 08:43:22 +0300

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = https://ljypbnvzjheztuwtajcn.supabase.coimport.meta.env.VITE_SUPABASE_URL as string | undefined;
-const anonKey =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqeXBibnZ6amhlenR1d3RhamNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NDczOTIsImV4cCI6MjEwMDIyMzM5Mn0.RpQ7zTmsdSqNyatnl5Xlzb8jkwygF4C0Xpg-oEXHdNk import.meta.env.VITE_SUPABASE_ANON_KEY as string | undefined;
+const url = port.meta.env.VITE_SUPABASE_URL as string | undefined;
+const anonKey import.meta.env.VITE_SUPABASE_ANON_KEY as string | undefined;
 
 if (!url || !anonKey) {
   // eslint-disable-next-line no-console
